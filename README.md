32a8bef3f64361caf25dc6bd9c99fca24a605788
Unknown <287382545@qq.com>
2019-04-03 15:48:55 +0800

    add a simple CNN of MNIST dataset

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 DNN简单网络模型
 
-> 1. [基于Keras的简单全连接网络](./dnn/KerasNet/FullyConnectedNetwork.py)
+> 1. [基于Keras的简单全连接网络](./dnn/KerasNet/FullyConnectedNetwork_MNIST.py)
+> 2. [基于Keras的简单卷积网络](./dnn/KerasNet/ConvolutionalNeuralNetwork_MNIST.py)
--- dnn/KerasNet/ConvolutionalNeuralNetwork_MNIST.py
+++ dnn/KerasNet/ConvolutionalNeuralNetwork_MNIST.py
@@ -1,45 +1,49 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-# 说明：　该程序是利用keras设计的一个包含N个隐藏层的神经网络。
-#        其中, N主要由hide_layer_size参数决定
+# 说明：　该程序是利用keras实现了一个包含2个卷积+2个全连接+一个softmax的神经网络。
+#
 # 数据集： MNIST
 
 import numpy as np
 import tensorflow.keras as keras
 import tensorflow.keras.datasets.mnist as mnist
-import livelossplot  # from livelossplot.keras import PlotLossesCallback  # pip install livelossplot
+from   livelossplot.keras import PlotLossesCallback  # pip install livelossplot
 
 
-class MNISTFullyConnectedNetwork(object):
+class ConvolutionalNeuralNetwork(object):
     """docstring for MNIST_FullyConnectedNetwork"""
+
+    input_shape = 0
+
     def __init__(self):
         self.model = keras.Sequential()
-        self.plot_losses = livelossplot.keras.PlotLossesCallback()
+        self.plot_losses = PlotLossesCallback()
 
-    def load_design_train_same(self,  hide_layer_size=(512, 512)):
+    def load_design_train_same(self, hide_layer_size=(512, 512)):
         # 加载数据
-        (x_train, y_train), (x_test, y_test) = MNISTFullyConnectedNetwork.read()
+        (x_train, y_train), (x_test, y_test) = self.read()
 
         print(x_train.shape)
         print(y_train.shape)
 
-        # 元组相加：　向隐藏层前后分别增加输入层和输出层节点数
-        layer_size = (x_train.shape[1],) + hide_layer_size + (y_train.shape[1],)
-
         # 设计网络模型
-        self.design(layer_size)
+        self.design()
 
         # 训练模型
-        self.train(x_train, y_train, x_test, y_test)
+        self.train(x_train, y_train, x_test, y_test, 128, 5)
 
         # 保存模型
-        self.save_model()
+        self.save_model(filename='model/convolutional_neural_network_model.h5')
 
-    # 加载数据,静态成员方法, Note: class method and cls
-    @classmethod
-    def read(cls, num_classes=10):
+    # 加载数据
+    def read(self, num_classes=10):
         (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+        # 将图片reshape为三维矩阵
+        self.input_shape = (x_train.shape[1], x_train.shape[2], 1)
+        x_train = x_train.reshape(x_train.shape[0], x_train.shape[1], x_train.shape[2], 1)
+        x_test = x_test.reshape(x_test.shape[0], x_test.shape[1], x_test.shape[2], 1)
 
         x_train = x_train.astype('float32')
         x_test = x_test.astype('float32')
@@ -48,9 +52,6 @@ class MNISTFullyConnectedNetwork(object):
         x_train /= 255
         x_test /= 255
 
-        x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1]*x_train.shape[2]))
-        x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1]*x_test.shape[2]))
-
         # 将类别向量(从0到nb_classes的整数向量)映射为二值类别矩阵
         y_train = keras.utils.to_categorical(y_train, num_classes)
         y_test = keras.utils.to_categorical(y_test, num_classes)
@@ -58,26 +59,36 @@ class MNISTFullyConnectedNetwork(object):
         return (x_train, y_train), (x_test, y_test)
 
     # 定义基于全连接网络的mnist分类模型
-    def design(self, layer_size):
-        if len(layer_size) <= 2:
-            print("Inappropriate parameter: lay_size, len(lay_size) should be larger than 2")
-            return False
+    def design(self):
+        # if len(layer_size) <= 2:
+        #     print("Inappropriate parameter: lay_size, len(lay_size) should be larger than 2")
+        #     return False
 
-        # model.add(keras.layers.Dense(layer_size[1], input_shape=(layer_size[0],), activation= keras.layers.Activation('relu')))
-        self.model.add(keras.layers.Dense(layer_size[1], input_shape=(layer_size[0],)))
+        # 第一层()
+        conv1 = keras.layers.Convolution2D(32, kernel_size=(3, 3), strides=(1, 1), input_shape=self.input_shape)
+        self.model.add(conv1)
         self.model.add(keras.layers.Activation('relu'))
-        self.model.add(keras.layers.Dropout(0.3))
 
-        # 隐藏层
-        for sz in layer_size[2:-1]:
-            self.model.add(keras.layers.Dense(sz))
-            self.model.add(keras.layers.Activation('relu'))
-            self.model.add(keras.layers.Dropout(0.3))
+        # 第二层
+        self.model.add(keras.layers.Convolution2D(32, (3, 3)))
+        self.model.add(keras.layers.Activation('relu'))
+        self.model.add(keras.layers.MaxPooling2D())
+        self.model.add(keras.layers.Dropout(0.25))  # Dense()的前面要减少连接点，防止过拟合，故通常要Dropout层或池化层
 
+        # 第三层()
+        self.model.add(keras.layers.Flatten())      # Dense()层的输入通常是2D张量，故应使用Flatten层或全局平均池化
+        self.model.add(keras.layers.Dense(128))
+        self.model.add(keras.layers.Activation('relu'))  # Dense()层的后面通常要加非线性化函数
+        self.model.add(keras.layers.Dropout(0.5))
+
+        # 第四层
         self.model.add(keras.layers.Dense(10))
-        self.model.add(keras.layers.Activation('softmax'))
 
-        self.model_info()
+        # 第五层
+        self.model.add(keras.layers.Activation('softmax'))  # 分类网络最后层,通常是softmax
+
+        self.model_info(to_file = 'image/convolutional_neural_network_model.png')
+
         return True
 
     def model_info(self, to_file='image/fully_connected_network_model.png'):
@@ -87,7 +98,7 @@ class MNISTFullyConnectedNetwork(object):
     # 训练和评估已定义好的模型
     def train(self, x_train, y_train, x_test, y_test, batch_size=128, nb_epoch=10):
         self.model.compile(loss='categorical_crossentropy',
-                           optimizer=keras.optimizers.RMSprop(),
+                           optimizer='adadelta',
                            metrics=['accuracy'])
 
         self.model.fit(x_train, y_train,
@@ -105,9 +116,9 @@ class MNISTFullyConnectedNetwork(object):
     # 根据模型获取预测结果，为了节约计算内存，也是分组（batch）加载到内存
     def predict(self, x, batch_size=128, verbose=0):
         result = self.model.predict(x, batch_size=batch_size, verbose=verbose)
-        
+
         # axis=1表示按行 取最大值   如果axis=0表示按列 取最大值 axis=None表示全部
-        result_max = np.argmax(result, axis=1)  
+        result_max = np.argmax(result, axis=1)
 
         return result_max
 
@@ -122,7 +133,7 @@ class MNISTFullyConnectedNetwork(object):
 
 
 def run(retrain=True):
-    fcn = MNISTFullyConnectedNetwork()
+    fcn = ConvolutionalNeuralNetwork()
     if retrain:
         fcn.load_design_train_same((512,))  # 传入不同的隐藏层节点数(512, 512, 256)
     else:
@@ -130,8 +141,8 @@ def run(retrain=True):
 
     _, (x_test, y_test) = fcn.read()
 
-    # 识别单个数据
-    single_num = fcn.predict(x_test[1].reshape((1, 784)))
+    # 识别单个数据, 切记首先要reshape数据
+    single_num = fcn.predict(x_test[1].reshape((1, 28, 28, 1)))
     print("single_num:", single_num)
 
     # 识别多个数据
@@ -140,6 +151,5 @@ def run(retrain=True):
 
 
 if __name__ == '__main__':
-
     # 传入参数True表示重新训练模型,否False时,将从model/mnist-fully-connected-network.h5中加载模型
     run(True)
